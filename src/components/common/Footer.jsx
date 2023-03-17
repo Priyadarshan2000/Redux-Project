97b1675be916d830cc232e1c68eeb3565c8bc3ba
Priyadarshan Ghosh <62868878+Priyadarshan2000@users.noreply.github.com>
2023-03-17 09:35:04 +0530

    Redux Project Update ✅

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>Copyright © Priyadarshan all rights reserved. Powered by Priyadarshan</p>
+      </footer>
+    </>
+  )
+}
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { Banner } from "./banner/Banner"
+import { Details } from "./details/Details"
+import { Card } from "./Hero/Card"
+import { Hero } from "./Hero/Hero"
+import { Product } from "./product/Product"
+import { TopProduct } from "./top/TopProduct"
+
+export const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Card />
+      <Product />
+      <Banner />
+      <TopProduct />
+    </>
+  )
+}
